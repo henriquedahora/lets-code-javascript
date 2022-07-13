e605ad4ee8aa9f68d27da3e1523360d6b75ae0e5
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-07-13 16:18:57 -0300

    Update operadores_booleanos.js

--- operadores_booleanos.js
+++ operadores_booleanos.js
@@ -22,8 +22,27 @@ console.log(numero != 10);          // Verifica se apenas o CONTEÚDO é igual (
 console.log(numero !== 10);         // Verifica se a VARIÁVEL e o CONTEÚDO são iguais (Tipo Number e 10).
 console.log(numero !== '10');       // Verifica se a VARIÁVEL e o CONTEÚDO são iguais (Tipo Number e 10, Tipo String e 10).
 
+
+
 // CONJUÇÕES LÓGICAS:
 
 // AND => &&
+// Se tiver maior ou igual a 18 anos e tiver CNH, então pode dirigir:
+let idade = 26;
+let tenhoCNH = true;
+const possoDirigir = idade >= 18 && tenhoCNH === true;
+console.log('Posso dirigir?', possoDirigir)
 
 // OR => ||
+// Se tiver menos de 18 anos ou mais de 70 anos, não precisa votar:
+let suaIdade = 20;
+const votoFacultativo = suaIdade < 18 || suaIdade >= 70;
+console.log('Meu voto é facultativo?:', votoFacultativo);
+
+console.clear()                     // Clear o Console até aqui!!!
+
+// NOT => !
+// Usado para inverter
+const estouGostandoDoCurso = false;
+console.log(estouGostandoDoCurso)       // Apresenta o valor real da variável, neste caso False.
+console.log(!estouGostandoDoCurso)      // Apresenta o valor invertido da variável, neste caso True.
