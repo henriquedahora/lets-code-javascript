34bc5224ef957b402e1dde59ce1c6379f23665fa
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-07-13 16:01:25 -0300

    Update operadores_booleanos.js

--- operadores_booleanos.js
+++ operadores_booleanos.js
@@ -15,8 +15,12 @@ console.log(numero >= 10);          // Maior ou Igual
 console.log(numero < 10);           // Menor que
 console.log(numero <= 10);          // Menor ou Igual 
 
-console.log(numero === '10')
+console.log(numero == '10')         // Verifica se apenas o CONTEÚDO é igual.
+console.log(numero === '10')        // Verifica se a VARIÁVEL e o CONTEÚDO são iguais.
 
+console.log(numero != 10);          // Verifica se apenas o CONTEÚDO é igual (Conteúdo 10).
+console.log(numero !== 10);         // Verifica se a VARIÁVEL e o CONTEÚDO são iguais (Tipo Number e 10).
+console.log(numero !== '10');       // Verifica se a VARIÁVEL e o CONTEÚDO são iguais (Tipo Number e 10, Tipo String e 10).
 
 // CONJUÇÕES LÓGICAS:
 
