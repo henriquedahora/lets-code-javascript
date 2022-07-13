a5f7c591c85720c52e0c4b8339307f7f8c1af093
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-07-13 10:32:34 -0300

    Update variaveis.js

--- variaveis.js
+++ variaveis.js
@@ -1,2 +1,24 @@
 // VARIÁVEIS:
+// O "let" substituio o "var" a partir de 2015.
+// A variável "const" é para variáveis constantes, ou seja, aquelas que não podem ser alteradas.
 
+let idade = 35;             // Criando a primeira variável
+console.log(idade);         // Imprimindo a variável "idade" no console
+
+// TIPOS DE VARIÁVEIS (Tipos Primitivos):
+
+let numero = 30;            // Tipo Number
+let altura = 1.77;          // Tipo Number
+let nome = 'Henrique';      // Tipo String (texto)
+let estudando = true;       // Tipo Booleano (true ou false)
+
+console.log(numero, typeof numero);         // Imprimindo a variável "numero" e também o tipo.
+console.log(altura, typeof altura);         // Imprimindo a variável "altura" e também o tipo.
+console.log(nome, typeof nome);             // Imprimindo a variável "nome" e também o tipo.
+console.log(estudando, typeof estudando);   // Imprimindo a variável "estudando" e também o tipo.
+
+let outra = undefined;                      // "undefined" também é um tipo de variável.
+console.log(outra)
+
+let nula = null;                            // "null" também é um tipo de variável.
+console.log(nula)
