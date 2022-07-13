ef11cd8ff6ebbfe9c566a2137b67d357e90c4ccb
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-07-13 11:20:22 -0300

    Operadores Aritmeticos

---
+++ operadores_aritmeticos.js
@@ -0,0 +1,32 @@
+// OPERADORES ARITMÉTICOS:
+
+/*
+Dicas para nomes de variáveis:
+- Nunca começe o nome da variável com números;
+- Sempre começe com letras;
+- Sempre começo com letras minúsculas;
+- Nuca use espaço;
+- Use a boa prática do "camelCase" (ex. primeiroNumero)
+*/
+
+let primeiroNumero = 20;
+let segundoNumero = 30;
+
+// Adição:              +
+// Subtração:           -
+// Multiplicação:       *
+// Divisão:             /
+// Exponenciação:       **
+// Resto da Divisão:    %
+
+console.log('Adição =', primeiroNumero + segundoNumero);
+console.log('Subtração =', primeiroNumero - segundoNumero);
+console.log('Multiplicação =', primeiroNumero * segundoNumero);
+console.log('Divisão =', primeiroNumero / segundoNumero);
+console.log('Exponenciação =', primeiroNumero ** segundoNumero);
+console.log('Resto da Divisão =', primeiroNumero % segundoNumero);
+
+// PRECEDÊNCIA DE OPERADORES:
+
+
+// BIBLIOTECA MATH:
