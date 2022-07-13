d932c6f97503c9cf4567212e2ec90bfa999100c2
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-07-13 12:07:32 -0300

    Create operadores_booleanos.js

---
+++ operadores_booleanos.js
@@ -0,0 +1,25 @@
+// OPERADORES BOOLEANOS:
+
+// Igualdade:           == (ou ===)
+// Desigualdade:        != (ou !==)
+// Maior que:           >
+// Maior ou Igual:      >=
+// Menor que:           <
+// Menor ou Igual:      <=
+
+const numero = 10;
+console.log(numero == 10);          // Igualdade
+console.log(numero != 10);          // Desigualdade
+console.log(numero > 10);           // Maior que
+console.log(numero >= 10);          // Maior ou Igual
+console.log(numero < 10);           // Menor que
+console.log(numero <= 10);          // Menor ou Igual 
+
+console.log(numero === '10')
+
+
+// CONJUÇÕES LÓGICAS:
+
+// AND => &&
+
+// OR => ||
