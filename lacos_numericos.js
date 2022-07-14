ef8d7fd9bc058cfbaefb5dd193d2b5d1d4eb1816
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-07-14 09:21:17 -0300

    Update lacos_numericos.js

--- lacos_numericos.js
+++ lacos_numericos.js
@@ -1,8 +1,11 @@
 // LAÇOS NUMÉRICOS: FOR
 
-// O problema:
-
-
+// O problema: Quero que você pegue 3 notas de um aluno e calcule a média.
+const input = require('readline-sync')
+const nota1 = Number(input.question('Informe a nota 1: '))
+const nota2 = Number(input.question('Informe a nota 2: '))
+const nota3 = Number(input.question('Informe a nota 3: '))
+let media = (nota1 + nota2 + nota3) / 3
 
 // Acumuladores:
 
