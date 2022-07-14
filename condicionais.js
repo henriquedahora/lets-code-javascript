c9f81e657ecb2ded01b2f3fc3f4cf8fcd3734bc4
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-07-13 21:30:03 -0300

    Update condicionais.js

--- condicionais.js
+++ condicionais.js
@@ -8,3 +8,18 @@ if (idade >= 18) {
 } else {
     console.log('Você é menor de idade!');
 }
+
+
+// Se media >= 7, Aprovado.
+// Se media < 7 e media >= 5, Recuperação.
+// Se media < 5,, Reprovado.
+
+console.clear()
+let media = 7;
+if (media >= 7) {
+    console.log('Aprovado!');
+} else if (media < 7 && media >= 5) {
+    console.log('Recuperação!')
+} else {
+    console.log('Reprovado!')
+}
